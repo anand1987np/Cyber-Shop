ced831bb8d35a2513854c2bde8264faaf282e878
anand1987np <79599818+anand1987np@users.noreply.github.com>
2021-12-22 14:31:01 +0530

    Update Footer.jsx

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,24 +1,32 @@
-import { push } from "connected-react-router";
-import React from "react";
-import { useDispatch } from "react-redux";
-const Footer = ({ price }) => {
-  const dispatch = useDispatch();
+import React, { useEffect, useState } from "react";
+
+export default function Footer({ price }) {
+  let pageUrl = window.location.toString();
+  const [showCheckoutButton, setShowCheckoutButton] = useState(true);
+  const key = localStorage.getItem("LOGIN_USER_KEY");
+
+  useEffect(() => {
+    if (pageUrl.includes("cart")) {
+      setShowCheckoutButton(false);
+    }
+  }, []);
+
   return (
     <footer>
-      <div className="subtotal">
-        <span className="subtotal-test">Subtotal:</span>
-        <span className="subtotal-price">${price}</span>
-      </div>
-
-      <button
-        onClick={() => {
-          dispatch(push("/shipping"));
-        }}
-      >
-        Check Out
-      </button>
+      {key != null && (
+        <div class="subtotal">
+          <span>Subtotal: ${price}</span>
+          {showCheckoutButton ? (
+            <a href="/cart">
+              <button>Check your Cart</button>
+            </a>
+          ) : (
+            <a href="/Shipping">
+              <button>Checkout</button>
+            </a>
+          )}
+        </div>
+      )}
     </footer>
   );
-};
-
-export default Footer;
+}
